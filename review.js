ba7d32f6756848eb657d914c5b20f8a082b9fd91
jhraju <jhraju890@gmail.com>
2024-03-02 01:02:23 +0600

    added keyboard enter function with keyup

--- review.js
+++ review.js
@@ -8,9 +8,22 @@ document.getElementById("submit-button").addEventListener("click", function(){
 
     const container = document.getElementById("review");
     const p = document.createElement("p");
-    p.innerText =serialCount + ". " + inputValue;
+    p.innerText =inputValue;
     
     container.appendChild(p);
-    serialCount++;
     document.querySelector("textarea").value = "";
+
+
+    document.getElementById("text-area").addEventListener("keyup", function(event){
+        if(event.key ==="Enter"){
+            const inputValue = document.querySelector("textarea").value;
+            console.log(inputValue);
+            const container = document.getElementById("review")
+            const p =document.createElement("p");
+            p.innerText = inputValue;
+            container.appendChild(p);
+            document.querySelector("textarea").value = "";
+
+        }
+    })
 });
